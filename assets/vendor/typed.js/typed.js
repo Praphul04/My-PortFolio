875358c1f4ed480c0e4cf92c46eebaed45f838d1
Praphul04 <praphulthakur6@gmail.com>
2024-06-16 12:06:20 +0530

    My portfolio

--- assets/vendor/typed.js/typed.js
+++ assets/vendor/typed.js/typed.js
@@ -1,4 +1,11 @@
+/*!
+ * 
+ *   typed.js - A JavaScript Typing Animation Library
+ *   Author: Matt Boldt <me@mattboldt.com>
+ *   Version: v2.0.12
 
+ * 
+ */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
